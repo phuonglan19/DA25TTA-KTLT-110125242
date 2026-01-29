053bdc4d9294b6b4d7ce7a886cc9c9958c0f3480
phuonglan19 <lan313805@gmail.com>
2026-01-29 08:31:03 +0700

    Add files via upload

---
+++ 110125242.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	int tong, hieu, tich;
+	float thuong;
+	
+	//Nhap so nguyen a,b
+	printf("Nhap vao so nguyen a= ");
+	scanf("%d", &a);
+	printf("Nhap vao so nguyen b= ");
+	scanf("%d", &b);
+	
+	printf("\nGia tri a = %d", a);
+	printf("\nGia tri b = %d", b);
+	
+	//Tinh tong
+	tong=a+b;
+	printf("\nTong a+b = %d", tong);
+	
+	hieu=a-b;
+	printf("\nHieu a-b = %d", hieu);
+	
+	tich=a*b;
+	printf("\nTich a*b = %d", tich);
+	
+	thuong=a/b;
+	printf("\nThuong a/b = %d", thuong);
+	
+	
+	return 0;
+}
+
+
+
+
